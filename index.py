b37bebdbae046cd9648eca70a29f47ed03e3d3b6
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-03-25 22:14:50 -0400

    Update

--- index.py
+++ index.py
@@ -28,17 +28,6 @@ class Usuario:
         self.pais = pais
         self.genero = genero
 
-    def mostrar_atributos(self):
-        print(f"Nombre: {self.nombre} {self.apellido}")
-        print(f"Fecha de Nacimiento: {self.fecha}")
-        print(f"Pais: {self.pais}")
-        print(f"Sexo: {self.genero}")
-        print(f"Informe:"
-              f"\tCondicion: {self.informe['condition']}"
-              f"\tStatus: {self.informe['status']}"
-              f"\tVirus: {self.informe['virus']}"
-              )
-
     def virus_status(self):
         if self.informe["status"]:
             if self.informe["virus"]:
@@ -53,19 +42,22 @@ class Usuario:
 
 
 def registrar_usuario():
+    print()
     datos = [
-        input("Nombre: ").strip(),
-        input("Apellido: ").strip(),
-        input("Fecha de Nacimiento: ").strip(),
-        input("Pais de Residencia: ").strip(),
-        input("Sexo: ").strip()
+        input("Nombre: ").strip().capitalize(),
+        input("Apellido: ").strip().capitalize(),
+        input("Fecha de Nacimiento: ").strip().capitalize(),
+        input("Pais de Residencia: ").strip().capitalize(),
+        input("Sexo: ").strip().capitalize()
     ]
     nuevo_usuario = Usuario(datos[0], datos[1], datos[2], datos[3], datos[4])
     return nuevo_usuario
 
 
-def mostrar_menu():
+def mostrar_menu_principal():
     opcion_valida = False
+    print(end="\n\n")
+    print("Bienvenido al registro de Covid-19")
     while not opcion_valida:
         print(" 1. Registrar Caso Sospechoso.")
         print(" 2. Modificar Casos Registrados.")
@@ -85,7 +77,117 @@ def mostrar_menu():
     return opcion
 
 
-def chequear_estado_paciente(usuario):
+def mostrar_menu_opciones():
+    opcion_valida = False
+    print(end="\n\n")
+    print("Por favor escoja una opcion para continuar:")
+    while not opcion_valida:
+        print(" 1. Sospechosos.")
+        print(" 2. Descartados.")
+        print(" 3. Contagiados.")
+        print(" 4. Curados.")
+        print(" 5. Fallecidos.")
+        try:
+            opcion = int(input("Opcion: "))
+        except ValueError:
+            print("\"Escoja una opcion valida\"")
+        else:
+            if 1 <= opcion <= 5:
+                opcion = "sospechosos" if opcion == 1 else "descartados" if opcion == 2 else "contagiados" if opcion == 3 else "curados" if opcion == 4 else "fallecidos"
+                opcion_valida = True
+            else:
+                print("\"Escoja una opcion valida\"")
+    return opcion
+
+
+def mostrar_usuarios(registros, opcion):
+    print(end="\n\n")
+    for usuario in registros[opcion]:
+        print(f"Nombre: {usuario.nombre} {usuario.apellido}")
+        print(f"Fecha de Nacimiento: {usuario.fecha}")
+        print(f"Pais: {usuario.pais}")
+        print(f"Sexo: {usuario.genero}")
+        print(f"Informe")
+        condicion = "Vivo" if usuario.informe["condition"] else "Muerto"
+        print(f"\tCondicion: {condicion}")
+        estado = "Vivo" if usuario.informe["status"] else "En Analisis"
+        print(f"\tStatus: {estado}")
+        virus = "Positivo" if usuario.informe["virus"] else "Negativo"
+        print(f"\tVirus: {virus}")
+        print()
+
+
+def editar_usuarios(registros, opcion):
+    contador = 0
+    print(end="\n\n")
+    print("Indique el Numero que desea modificar: ")
+    for usuario in registros[opcion]:
+        contador += 1
+        print(f"{contador}. {usuario.nombre} {usuario.apellido}")
+    while True:
+        try:
+            caso = int(input("Caso # "))
+        except ValueError:
+            print("\"Escoja una opcion valida\"")
+        else:
+            if 1 <= caso <= 2:
+                break
+            else:
+                print("\"Escoja una opcion valida\"")
+
+    print(end="\n\n")
+    print(f"{registros[opcion][caso - 1].nombre} {registros[opcion][caso - 1].apellido}")
+    opcion_valida = False
+    while not opcion_valida:
+        print("Estado del Usuario:")
+        print("1. Vivo")
+        print("2. Fallecido")
+        try:
+            opcion_seleccionada = int(input("Opcion: "))
+        except ValueError:
+            print("\"Escoja una opcion valida\"")
+        else:
+            if 1 <= opcion_seleccionada <= 2:
+                registros[opcion][caso - 1].informe["condition"] = True if opcion_seleccionada == 1 else False
+                opcion_valida = True
+            else:
+                print("\"Escoja una opcion valida\"")
+    print()
+    opcion_valida = False
+    while not opcion_valida:
+        print("Estado del Analisis:")
+        print("1. En Proceso")
+        print("2. Con Resultados")
+        try:
+            opcion_seleccionada = int(input("Opcion: "))
+        except ValueError:
+            print("\"Escoja una opcion valida\"")
+        else:
+            if 1 <= opcion_seleccionada <= 2:
+                registros[opcion][caso - 1].informe["status"] = False if opcion_seleccionada == 1 else True
+                opcion_valida = True
+            else:
+                print("\"Escoja una opcion valida\"")
+    print()
+    opcion_valida = False
+    while not opcion_valida:
+        print("Estado del Virus:")
+        print("1. Positivo")
+        print("2. Negativo")
+        try:
+            opcion_seleccionada = int(input("Opcion: "))
+        except ValueError:
+            print("\"Escoja una opcion valida\"")
+        else:
+            if 1 <= opcion_seleccionada <= 2:
+                registros[opcion][caso - 1].informe["virus"] = False if opcion_seleccionada == 1 else True
+                opcion_valida = True
+            else:
+                print("\"Escoja una opcion valida\"")
+    print()
+
+
+def chequear_estado_usuarios(usuario):
     if usuario.informe["condition"]:  # Si esta vivo
         if usuario.informe["status"]:  # Ya tiene resultado de los analisis
             if (usuario in registros["contagiados"]) and not usuario.informe["virus"]:  # No es portador del virus
@@ -101,7 +203,9 @@ def chequear_estado_paciente(usuario):
             registros["sospechosos"].remove(usuario)
         if usuario in registros["recuperados"]:
             registros["sospechosos"].remove(usuario)
-        registros["fallecidos"].append(usuario)
+        if not (usuario in registros["fallecidos"]):  # Si no esta en la lista de fallecidos
+            registros["fallecidos"].append(usuario)
+
         print("El paciente ha fallecido.")
 
 
@@ -116,30 +220,41 @@ def volver_al_menu_msg():
 
 registros = {
     "sospechosos": [],
+    "descartados": [],
     "contagiados": [],
     "recuperados": [],
     "fallecidos": [],
     "sintomas": {
-        "covid19": ["secreciones nasales", "dolor de garganta", "tos", "fiebre", "dificultad para respirar"],
-        ""
+        "Corona Virus": ["Tos Seca", "Fiebre", "Cansancio", "Congestion Nasal", "Secrecion Nasal", "Dolor de Garganta",
+                         "Dolor de Cabeza", "Diarrea", "Dificultad para Respirar"],
+        "Resfriado Comun": ("Tos", "Fiebre", "Congestion Nasal", "Secrecion Nasal", "Dolor de Garganta",
+                            "Dolor de Cabeza", "Dolor Muscular", "Cansancio"),
+        "Alergia": ("Estornudos", "Congestion Nasal", "Secrecion Nasal", "Picazon", "Irritacion Ocular")
     }
 }
 
-print(end="\n\n")
-print("Bienvenido al registro de Covid-19")
+
 while True:
-    opcion_seleccionada = mostrar_menu()
+
+    opcion_seleccionada = mostrar_menu_principal()
+
     if opcion_seleccionada == 1:  # Registrar Usuario Sospechoso
-        desea_continuar = True
-        while desea_continuar:
+        volver_al_menu = False
+        while not volver_al_menu:
             try:
                 registros["sospechosos"].append(registrar_usuario())
             except:
-                print("Hubo un error al registrar el usuario por favor intentelo de nuevo")
+                print(end="\n\n")
+                print("Hubo un Error al registrar el usuario por favor intentelo de nuevo")
+                print(end="\n\n")
             else:
-                print("\t Se ha registrado el usuario exitosamente en la lista de sospechosos.", end="\n\n")
-                desea_continuar = volver_al_menu_msg()
+                print("\t Se ha registrado el usuario exitosamente en la lista de Sospechosos.", end="\n\n")
+                volver_al_menu = volver_al_menu_msg()
+
     elif opcion_seleccionada == 2:  # Modificar Casos
+        opcion_modificar = mostrar_menu_opciones()
+        editar_usuarios(registros, opcion_modificar)
 
     else:  # Mostrar Registros
-        pass
+        opcion_mostrar = mostrar_menu_opciones()
+        mostrar_usuarios(registros, opcion_mostrar)
